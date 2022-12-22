bb1b9a09a9acb2582ee721642a25262718c84cda
Andrei Tokmakov <and-tokmakov@yandex-team.ru>
2022-12-22 16:02:37 +0400

    Some more changes

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import platform
+import re
+import socket
+import uuid
+
+import psutil
 
 if __name__ == '__main__':
 
@@ -9,3 +14,16 @@ if __name__ == '__main__':
     print(f'Release: {platform.release()}')
     print(f'System: {platform.system()}')
     print(f'Processor: {platform.processor()}')
+
+    hostname: str = socket.gethostname()
+    ipaddress: str = socket.gethostbyname(hostname)
+    mac: str = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
+
+    print(f'\nHostname: {hostname}')
+    print(f'IP Address: {ipaddress}')
+    print(f'MAC Address: {mac}')
+
+    ram = str(round(psutil.virtual_memory().total / (1024.0 ** 3))) + " GB"
+    print(f'RAM Total: {ram}')
+
+    pass
