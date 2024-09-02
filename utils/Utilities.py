863aaefdc7bd6da37abf50d48b34d2a749ed7283
AndreyTokmakov <andtokm@yandex.ru>
2024-09-02 14:15:47 +0400

    Super mega changes

--- utils/Utilities.py
+++ utils/Utilities.py
@@ -5,6 +5,9 @@ class Calculator(object):
     def add(self, a: int, b: int) -> int:
         return a + b
 
+    def info(self):
+        print('INFO')
+
     def minus(self, a: int, b: int) -> int:
         return a - b
 
